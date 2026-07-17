94f3838df509e0e6ac3291b054f21c68ba7d339d
ImJunaidKhan <mjunaidkhan996@outlook.com>
2026-07-17 05:10:53 +0500

    Assignment 10 - JS Basics 1-9 chapters

---
+++ Assignment 10 - JS Beginner/app.js
@@ -0,0 +1,291 @@
+// Chapter 1 - Alerts
+// alert("Muhammad Junaid");
+// alert("Khan");
+// alert("mjunaidkhan996@gmail.com");
+// alert("03123456789");
+// alert("Pakistan123@");
+
+// alert("You're learning Javascript!");
+
+// alert("Lets begin programming!");
+
+// Chapter 2 - Variables for strings
+// var myName;
+
+// var favColor;
+
+// favColor = "Yellow";
+
+// var teamName = "Spain";
+
+// alert(teamName);
+
+// var bestMan = "Charlie";
+
+// bestMan = "John";
+
+// Chapter 3 - Variables for numbers
+// var caseQty;
+
+// caseQty = 144;
+
+// var num = 9;
+
+// var sum = caseQty + num;
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+
+// var orderTotal = merchTotal + shippingCharge;
+
+// alert("The total order cost is " + orderTotal);
+
+// var age = 20;
+// age = 20 + 5;
+// alert(age);
+
+// Chapter 4 - Variable Names Legal and Illegal
+// var a, b, c;
+
+// var myName, my$Name, my_Name, _myName1, $myName2;
+// var 1stName, alert, my-Name, @name, total%;
+
+// Chapter 5 - Math expressions
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum2 = num1 + num2;
+
+// document.write("<h3>Sum of " + num1 + " and " + num2 + " is " + sum2 + "</h3>");
+
+// var sub = num1 - num2;
+
+// document.write("<h3>Difference of " + num1 + " and " + num2 + " is " + sub + "</h3>");
+
+// var mul = num1 * num2;
+
+// document.write("<h3>Product of " + num1 + " and " + num2 + " is " + mul + "</h3>");
+
+// var div = num1 / num2;
+
+// document.write("<h3>Division of " + num1 + " and " + num2 + " is " + div + "</h3>");
+
+// var mod = num1 % num2;
+
+// document.write("<h3>Modulus of " + num1 + " and " + num2 + " is " + mod + "</h3>");
+
+// var num3;
+
+// document.write("<h3>Value after variable declaration is " + num3 + "</h3>");
+
+// num3 = 15;
+
+// document.write("<h3>Initial value: " + num3 + "</h3>");
+
+// num3++;
+
+// document.write("<h3>Value after increment is " + num3 + "</h3>");
+
+// num3 = num3 + 7;
+
+// document.write("<h3>Value after addition is " + num3 + "</h3>");
+
+// num3--;
+
+// document.write("<h3>Value after decrement is " + num3 + "</h3>");
+
+// document.write("<h3>The remainder is " + (num3 % 3) + "</h3>");
+
+// var movieTicketPrice = 600;
+// var totalTickets = 5;
+// var totalCost = movieTicketPrice * totalTickets;
+
+// document.write("<h3>Total cost to buy " + totalTickets + " tickets to a movie is " + totalCost + " PKR</h3>");
+
+// document.write("<h2>Table of 7</h2>");
+// document.write("<h3>" + 7 + " x " + 1 + " = " + (7 * 1) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 2 + " = " + (7 * 2) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 3 + " = " + (7 * 3) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 4 + " = " + (7 * 4) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 5 + " = " + (7 * 5) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 6 + " = " + (7 * 6) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 7 + " = " + (7 * 7) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 8 + " = " + (7 * 8) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 9 + " = " + (7 * 9) + "</h3>");
+// document.write("<h3>" + 7 + " x " + 10 + " = " + (7 * 10) + "</h3>");
+
+// var celciusTemp = 42;
+// var fahrenheit = (celciusTemp * 9 / 5) + 32;
+
+// document.write("<h3>" + celciusTemp + "<sup>0</sup>C is " + fahrenheit + "<sup>0</sup>F</h3>");
+
+// var farhenheit2 = 102;
+// var celcius2 = (farhenheit2 - 32) * 5 / 9;
+
+// document.write("<h3>" + farhenheit2 + "<sup>0</sup>F is " + celcius2 + "<sup>0</sup>C</h3>");
+
+// var item1Price = 650;
+// var quantity1 = 3;
+// var item2Price = 100;
+// var quantity2 = 7;
+// var shippingCharge = 100;
+
+// document.write("<p>Price of Item 1 is " + item1Price + " <br></p>");
+
+// document.write("<p>Quantity of Item 1 is " + quantity1 + "<br></p>");
+
+// document.write("<p>Price of Item 2 is " + item2Price + "<br></p>");
+
+// document.write("<p>Quantity of Item 2 is " + quantity2 + "<br></p>");
+
+// document.write("<p>Shipping charges are " + shippingCharge + "<br></p>");
+
+// var totalCost = (item1Price * quantity1) + (item2Price * quantity2) + shippingCharge;
+
+// document.write("<h3>The total cost of your order is " + totalCost + "</h3>");
+
+
+// document.write("<h2>Mark Sheet</h2>");
+
+// var totalMark = 980;
+
+// var obtainedMark = 804;
+
+// var percentage = (obtainedMark / totalMark) * 100;
+
+// document.write("<h3>Total Marks: " + totalMark + "</h3>");
+// document.write("<h3>Marks Obtained: " + obtainedMark + "</h3>");
+// document.write("<h3>Percentage: " + percentage + "%</h3>");
+
+
+// document.write("<h2>Currency Conversion</h2>");
+
+// var usdAmount = 10;
+// var usdExchangeRate = 280;
+// var usdToPkr = usdAmount * usdExchangeRate;
+
+// var saudiRiyal = 25;
+// var saudiExchangeRate = 75;
+// var saudiRiyalToPkr = saudiRiyal * saudiExchangeRate;
+
+// document.write("<h3>" + usdAmount + " USD is equal to " + usdToPkr + " PKR</h3>");
+
+// document.write("<h3>" + saudiRiyal + " SAR is equal to " + saudiRiyalToPkr + " PKR</h3>");
+
+// document.write("<h3>Total amount is " + (usdToPkr + saudiRiyalToPkr) + " PKR</h3>");
+
+
+// var originalNum = 30;
+// var newNum = ((originalNum + 5) * 10) / 2;
+
+// alert("The new number after calculation is " + newNum);
+
+
+// document.write("<h2>Age Calculator</h2>");
+
+// var currentYear = 2026;
+// var birthYear = 1998;
+// var age = currentYear - birthYear;
+
+// document.write("<p>Current Year: " + currentYear + "</p>");
+// document.write("<p>Birth Year: " + birthYear + "</p>");
+// document.write("<p>Your age is: " + age + "</p>");
+
+// document.write("<h2> Geometrizer</h2>");
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * radius * radius;
+
+// document.write("<p>Radius of a circle is: " + radius + "</p>");
+// document.write("<p>The circumference is: " + circumference + "</p>");
+// document.write("<p>The area is: " + area + "</p>");
+
+
+// document.write("<h2>The Lifetime Supply Calculator</h2>");
+
+// var favSnack = "Cheetos";
+// var currentAge = 26;
+// var maxAge = 65;
+// var snacksPerDay = 2;
+
+// var totalSnacks = (maxAge - currentAge) * 365 * snacksPerDay;
+
+// document.write("<p>Favorite Snack: " + favSnack + "</p>");
+// document.write("<p>Current Age: " + currentAge + "</p>");
+// document.write("<p>Estimated Maximum Age: " + maxAge + "</p>");
+// document.write("<p>Amount of snacks per day: " + snacksPerDay + "</p>");
+// document.write("<p>You will need " + totalSnacks + " " + favSnack + " to last you until the ripe old age of " + maxAge + "</p>");
+
+// Chapter 6 - Math expressions - Part 2
+
+// document.write("<h2>Result:</h2>");
+
+// var a = 10;
+// document.write("<p>The value of a is " + a + "</p><br/><hr/>");
+
+// document.write("<p>The value of ++a is " + ++a + "</p>");
+// document.write("<p>Now the value of a is " + a + "</p><br/>");
+
+// document.write("<p>The value of a++ is " + a++ + "</p>");
+// document.write("<p>Now the value of a is " + a + "</p><br/>");
+
+// document.write("<p>The value of --a is " + --a + "</p>");
+// document.write("<p>Now the value of a is " + a + "</p><br/>");
+
+// document.write("<p>The value of a-- is " + a-- + "</p>");
+// document.write("<p>Now the value of a is " + a + "</p>");
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// // result = 1 - 0 + 1 + 1 = 3
+
+// document.write("<p>a is " + a + "</p>");
+// document.write("<p>b is " + b + "</p>");
+// document.write("<p>result is " + result + "</p>");
+
+
+// var yourName = prompt("Enter your name");
+// alert("Welcome " + yourName + "!");
+
+
+// var myN = +prompt("Enter the number for which you want the table of", 5);
+
+// document.write("<h2>Table of " + myN + "</h2>");
+
+// document.write("<p>" + myN + " x 1 = " + myN*1 + "</p>");
+// document.write("<p>" + myN + " x 2 = " + myN*2 + "</p>");
+// document.write("<p>" + myN + " x 3 = " + myN*3 + "</p>");
+// document.write("<p>" + myN + " x 4 = " + myN*4 + "</p>");
+// document.write("<p>" + myN + " x 5 = " + myN*5 + "</p>");
+// document.write("<p>" + myN + " x 6 = " + myN*6 + "</p>");
+// document.write("<p>" + myN + " x 7 = " + myN*7 + "</p>");
+// document.write("<p>" + myN + " x 8 = " + myN*8 + "</p>");
+// document.write("<p>" + myN + " x 9 = " + myN*9 + "</p>");
+// document.write("<p>" + myN + " x 10 = " + myN*10 + "</p>");
+
+
+// document.write("<h2>Mark Sheet</h2>");
+
+// var sub1 = prompt("Enter First Subject Name:", "English");
+// var sub2 = prompt("Enter Second Subject Name:", "Math");
+// var sub3 = prompt("Enter Third Subject Name:", "Urdu");
+
+// var totalMarks = 100;
+// var obtMarks1 = +prompt("Enter marks for " + sub1 + ":", 54);
+// var obtperc1 = (obtMarks1 / totalMarks) * 100;
+// var obtMarks2 = +prompt("Enter marks for " + sub2 + ":", 54);
+// var obtperc2 = (obtMarks2 / totalMarks) * 100;
+// var obtMarks3 = +prompt("Enter marks for " + sub3 + ":", 48);
+// var obtperc3 = (obtMarks3 / totalMarks) * 100;
+
+// var grandTotalMarks = totalMarks * 3;
+// var totalObtMarks = obtMarks1 + obtMarks2 + obtMarks3;
+// var percentage = (totalObtMarks / grandTotalMarks) * 100;
+
+// document.write("<table><tr> <th>Subject</th> <th>Total Marks</th> <th>Obtained Marks</th> <th>Percentage</th> </tr>")
+// document.write("<tr><td>" + sub1 + "</td><td>" + totalMarks + "</td><td>" + obtMarks1 + "</td><td>" + obtperc1 + "%</td></tr>");
+// document.write("<tr><td>" + sub2 + "</td><td>" + totalMarks + "</td><td>" + obtMarks2 + "</td><td>" + obtperc2 + "%</td></tr>");
+// document.write("<tr><td>" + sub3 + "</td><td>" + totalMarks + "</td><td>" + obtMarks3 + "</td><td>" + obtperc3 + "%</td></tr>");
+// document.write("<tr><td>Total</td><td>" + grandTotalMarks + "</td><td>" + totalObtMarks + "</td><td>" + percentage + "%</td></tr></table>");
